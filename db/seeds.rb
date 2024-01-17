c873797bc6f3611be86c6735bf7093e586093fa1
juan bernal <mesieou@gmail.com>
2024-01-18 10:48:08 +1100

    added index and show pages with style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
 require 'faker'
 
-puts "creating articles:"
+puts 'creating articles:'
+
+counter = 0
+
 20.times do
-  counter = 0
-  puts(counter + 1)
-  article = Article.new(title: Faker::Book.title,
-              content: Faker::Lorem.paragraphs(number: 3).join("\n\n"))
+  puts(counter += 1)
+
+  number_of_paragraphs = 4
+  sentences_per_paragraph = 15
+
+  content = Array.new(number_of_paragraphs) do
+    Faker::Lorem.sentences(number: sentences_per_paragraph).join(' ')
+  end
+
+  content = content.join(",")
+
+  article = Article.new(title: Faker::Book.title, content: content)
   article.save
 end
