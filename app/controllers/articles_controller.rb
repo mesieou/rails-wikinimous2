04163ef8f2a54acbc70737c76d8bfc3be443f9fa
juan bernal <mesieou@gmail.com>
2024-01-18 14:19:59 +1100

    added new and create features for the model article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,17 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
   private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
   def set_article
     @article = Article.find(params[:id])
