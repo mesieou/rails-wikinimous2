9a942e239e19f5693f79d9003564eaa9496fc339
juan bernal <mesieou@gmail.com>
2024-01-19 09:38:06 +1100

    organised code a bit better

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  def show; end
+  def edit; end
+
   def index
     @articles = Article.all
   end
-
-  def show; end
 
   def new
     @article = Article.new
@@ -16,8 +17,6 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
-
-  def edit; end
 
   def update
     @article.update(article_params)
